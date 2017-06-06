9073a38d0c52f28c0f5551bbf680066999d63288
Brandon L.N. Gayle <bgayle786@gmail.com>
2017-06-05 21:26:30 -0400

    updated home page

--- js/main.js
+++ js/main.js
@@ -7,3 +7,14 @@ function menu(){
 		m.className = "nav-cont";
 	}
 }
+
+var fresh_btn = document.getElementById("trans-btn");
+
+function rand_work(){
+var site_arr = ["https://codepen.io/Freshhh/pen/xqRLOK","https://codepen.io/Freshhh/pen/xqRLOK" ,"https://codepen.io/Freshhh/pen/dpmJxr" ,"https://codepen.io/Freshhh/pen/mALPAm" ];
+var x = site_arr.length;
+var rand = Math.floor(Math.random() * x);
+var rand_site = site_arr[rand];
+window.open(rand_site);
+console.log(rand_site, rand, x);
+}
